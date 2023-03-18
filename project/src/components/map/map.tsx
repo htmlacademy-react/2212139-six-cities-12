83a966f0b61558714a01795ba3a4f0d44b65c43a
Makeev Alexey <makeevhome@mail.ru>
2023-03-18 16:24:28 +0300

    feat: not work map

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -66,7 +66,7 @@ function Map({className, offers}: MapProps): JSX.Element {
     <section
       className={clsx('map', {className})}
       ref={mapRef}
-      style={{height: '500px', maxWidth: '1144px', margin: '0 auto'}}
+      style={{height: '500px', width: '100%'}}
     >
     </section>
   );
