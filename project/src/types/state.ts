e4c8ef52382fc13bc33e9fb786da3502e64e007b
Makeev Alexey <makeevhome@mail.ru>
2023-04-09 20:05:55 +0300

    feat: add propertyData test

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -1,6 +1,7 @@
-import { AuthorizationStatus, FetchStatus, } from '../const.js';
-import {store} from '../store/index.js';
-import { Offers } from './offer.js';
+import { AuthorizationStatus, FetchStatus } from '../const.js';
+import { store } from '../store/index.js';
+import { Offer, Offers } from './offer.js';
+import { Reviews } from './review.js';
 import { UserData } from './user.js';
 
 export type State = ReturnType<typeof store.getState>;
@@ -14,4 +15,12 @@ export type UserDataState = {
 export type OffersDataState = {
   offers: Offers;
   offersStatus: FetchStatus;
+};
+
+export type OfferPropertyDataState = {
+  offerProperty: Offer | null;
+  offerPropertyStatus: FetchStatus;
+  nearOffers: Offers;
+  reviews: Reviews;
+  reviewFormBlockedStatus: FetchStatus;
 };
